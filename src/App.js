fa53097c4e4de913d4a5e8c95ff29ed9bbd4e98f
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-01 14:27:47 -0600

    Refactoring and component setup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
-import { GoBellFill, GoCheckCircle, GoEye } from "react-icons/go";
-// import * as GoIcons from "react-icons/go";
-import Button from "./Button";
-// console.log(GoIcons);
+// import Button from "./components/Button";
+import Accordion from "./components/Accordion";
+
 
 function App() {
-  const handleClick = () => {
-    console.log('Click!!');
-  };
-
-  return (
-    <div>
-      <div>
-        <Button success outline rounded className="mb-5" onClick={handleClick}>
-          <GoBellFill />
-          Click Me
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCheckCircle />
-          Buy Now
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoEye />
-          See Deal
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>Hide Ads</Button>
-      </div>
-      <div>
-        <Button primary rounded>Something!</Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      label: 'Can I use React on a Project?',
+      content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.'
+    },
+    {
+      label: 'Can I use JavaScript on a Project?',
+      content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.'
+    },
+    {
+      label: 'Can I use CSS on a Project?',
+      content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.'
+    },
+  ]
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,41 @@
+import { GoBellFill, GoCheckCircle, GoEye } from "react-icons/go";
+// import * as GoIcons from "react-icons/go";
+import Button from "./components/Button";
+// console.log(GoIcons);
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log('Click!!');
+  };
+
+  return (
+    <div>
+      <div>
+        <Button success outline rounded className="mb-5" onClick={handleClick}>
+          <GoBellFill />
+          Click Me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoCheckCircle />
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoEye />
+          See Deal
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>Hide Ads</Button>
+      </div>
+      <div>
+        <Button primary rounded>Something!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
