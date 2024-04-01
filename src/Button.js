8d9d338a5f9eca0487dfcb2836d510a84ce1d58b
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-01 13:11:34 -0600

    Fix issue with className

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,3 @@
-// Used classnames library to decide on the layout of the button variations -i.e if button is primary/secondary/warning/success/rounded/danger/outline what styles/classes to show etc
-// Used propTypes library to show warnings if we passed two variations like primary and secondary at the same time on a Button component. We can only pass one from the list - primary, secondary, success, warning, danger
 import className from 'classnames';
 
 function Button({
@@ -11,13 +9,14 @@ function Button({
   danger,
   outline,
   rounded,
+  onClick,
 }) {
   const classes = className('flex items-center px-3 py-1.5 border', {
-    'border-blue-500 bg-blue-500 text-white': primary,
-    'border-gray-900 bg-gray-900 text-white': secondary,
-    'border-green-500 bg-green-500 text-white': success,
-    'border-yellow-400 bg-yellow-400 text-white': warning,
-    'border-red-500 bg-red-500 text-white': danger,
+    'border-blue-500 bg-blue-500 text-white': primary && !outline,
+    'border-gray-900 bg-gray-900 text-white': secondary && !outline,
+    'border-green-500 bg-green-500 text-white': success && !outline,
+    'border-yellow-400 bg-yellow-400 text-white': warning && !outline,
+    'border-red-500 bg-red-500 text-white': danger && !outline,
     'rounded-full': rounded,
     'bg-white': outline,
     'text-blue-500': outline && primary,
@@ -25,15 +24,13 @@ function Button({
     'text-green-500': outline && success,
     'text-yellow-400': outline && warning,
     'text-red-500': outline && danger,
-    // 'bg-white border border-blue-500 text-blue-500': outline && primary,
-    // 'bg-white border border-gray-900 text-gray-900': outline && secondary,
-    // 'bg-white border border-green-500 text-green-500': outline && success,
-    // 'bg-white border border-yellow-400 text-yellow-400': outline && warning,
-    // 'bg-white border border-red-500 text-red-500': outline && danger,
-    // 'text-black': outline, // Set text color to black when outline is true
   });
 
-  return <button className={classes}>{children}</button>;
+  return (
+    <button onClick={onClick} className={classes}>
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
