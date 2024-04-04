89617716fff410b0d0db271d2f123803d708e108
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-04 15:14:58 -0600

    Adding styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,15 +16,16 @@ function Dropdown({ options, value, onChange }) {
   };
 
   const renderedOptions = options.map((option) => {
-    return <div key={option.value} onClick={() => handleOptionClick(option)}>{option.label}</div>
+    return <div key={option.value} onClick={() => handleOptionClick(option)} className="hover:bg-sky-100 rounded cursor-pointer p-1">{option.label}</div>
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>
+    <div className="w-48 relative">
+      <div onClick={handleClick} className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full">
         {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
       </div>
-      {isOpen && <div>{renderedOptions}</div>}
+      {isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
     </div>
   );
 }
