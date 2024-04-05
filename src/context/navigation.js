b039026881e61338da0865c30b4f54c1ec2a1a1f
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-05 15:59:09 -0600

    programmatic navigation

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -16,8 +16,17 @@ function NavigationProvider ({ children }) {
     }
   }, []);
 
+  const navigate = (to) => {
+    window.history.pushState({}, '', to);
+    setCurrentPath(to);
+  };
+
   return (
     <NavigationContext.Provider value={{}}>
+      <div>
+        <button onClick={() => navigate('/accordion')}>Go to accordion</button>
+        <button onClick={() => navigate('/dropdown')}>Go to dropdown</button>
+      </div>
       {currentPath}
       {children}
     </NavigationContext.Provider>
