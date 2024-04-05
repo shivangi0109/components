3ff5dd3c45c4b476de0da5c518b3f84ce7190e4f
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-05 14:19:15 -0600

    Extracting the dropdown page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import { useState } from "react";
-import Dropdown from './components/Dropdown';
-
 function App() {
-  const [selection, setSelection] = useState(null);
 
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red"},
-    { label: "Green", value: "green"},
-    { label: "Blue", value: "blue"},
-  ]
-  return <Dropdown options={options} value={selection} onChange={handleSelect} />
+  return <div>App</div>
 }
 
 export default App;
