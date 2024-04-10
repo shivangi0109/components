05641b259a6b7dadfb70e0b44139cf931d49c1bc
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-10 14:16:24 -0600

    Create modal to work with styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,11 @@
 function Modal() {
   return (
-    <div>modal!!</div>
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        I am a modal!!
+      </div>
+    </div>
   );
 }
 
