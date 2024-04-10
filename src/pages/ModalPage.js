60e0ba1a5946249af6d00c8d84d636b452fc0ce6
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-10 15:36:45 -0600

    Close the modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -9,10 +9,14 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  }
+
   return (
     <div className="relative">
       <Button primary onClick={handleClick}>Open Modal</Button>
-      {showModal && <Modal />}
+      {showModal && <Modal onClose={handleClose} />}
     </div>
   );
 }
