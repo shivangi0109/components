616fa1a5470ce562dd1efab1467bc9931d5da87b
Shivangi Srivastava <shi06dec@gmail.com>
2024-04-10 15:47:07 -0600

    Customize the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom';
 
-function Modal({ onClose }) {
+function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
       <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
       <div className="absolute inset-40 p-10 bg-white">
-        I am a modal!!
+        {children}
+        {actionBar}
       </div>
     </div>,
     document.querySelector('.modal-container')
