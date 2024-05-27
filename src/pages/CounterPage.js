cdb30e98257605829714e52b22677f9ec61cfc22
Shivangi Srivastava <shi06dec@gmail.com>
2024-05-27 14:39:50 -0600

    Rules for reducer functions

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -3,7 +3,10 @@ import Button from '../components/Button';
 import Panel from '../components/Panel';
 
 const reducer = (state, action) => {
-  //
+  return {
+    ...state,
+    count: state.count + 1,
+  }
 };
 
 function CounterPage({ initialCount }) {
@@ -15,7 +18,7 @@ function CounterPage({ initialCount }) {
   });
 
   const increment = () => {
-    // setCount(count + 1);
+    dispatch();
   };
 
   const decrement = () => {
