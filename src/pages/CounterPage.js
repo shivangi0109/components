4bab78e248bf0f68426d37c738852d959976334d
Shivangi Srivastava <shi06dec@gmail.com>
2024-05-27 15:26:04 -0600

    Constant action types

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,15 +2,18 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'change-value-to-add';
+
 const reducer = (state, action) => {
-  if (action.type === 'increment') {
+  if (action.type === INCREMENT_COUNT) {
     return {
       ...state,
       count: state.count + 1,
     }
   }
 
-  if (action.type === 'change-value-to-add') {
+  if (action.type === SET_VALUE_TO_ADD) {
     return {
       ...state,
       valueToAdd: action.payload
@@ -32,7 +35,7 @@ function CounterPage({ initialCount }) {
 
   const increment = () => {
     dispatch({
-      type: 'increment'
+      type: INCREMENT_COUNT
     });
   };
 
@@ -44,7 +47,7 @@ function CounterPage({ initialCount }) {
     const value = parseInt(event.target.value) || 0;
 
     dispatch({
-      type: 'change-value-to-add',
+      type: SET_VALUE_TO_ADD,
       payload: value
     });
   };
